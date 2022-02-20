559eb0abb4f40229c6afd3a663a6dbbf07015309
Begemottt <55757703+Begemottt@users.noreply.github.com>
2022-02-19 20:57:04 -0800

    Nav Menu & Theme Button
    Added the main nav menu, which will be on every page EXCEPT the front, and also the theme switching button! You don't have to use the test one anymore, haha.

--- dist/main.js
+++ dist/main.js
@@ -1455,22 +1455,48 @@ const test_2 = () => {
         duration: 1000
     })
 }
-const test_3 = () => {
-    var element = document.getElementById('theme_wrapper');
-    element.classList.toggle("dark");
-    element.classList.toggle("light");
+// Theme swap function!
+const theme_button = document.getElementById('theme');
+const swap_theme = () => {
+    if(theme_button.classList.contains('active') === false){
+        activate(theme_button);
+        var element = document.getElementById('theme_wrapper');
+        element.classList.toggle("dark");
+        element.classList.toggle("light");
+        pulse(theme_button);
+    }
+}
+const pulse = (el) =>{
+    animate(el);
+    (0,animejs__WEBPACK_IMPORTED_MODULE_0__["default"])({
+        targets: el,
+        scale: 3,
+        direction: 'alternate',
+        duration: 400,
+        easing: 'easeOutQuad',
+        complete: function(anime){
+            animate(el);
+            activate(theme_button);
+        }
+    })
 }
 
 // Adds a class that gives an element the "will change: all" attribute, for smoother animations!
 const animate = (el) => {
     el.classList.toggle("animate");
 }
+const activate = (el) => {
+    el.classList.toggle("active");
+}
 
-
-
+// * [ Event Listeners ]
+// Test buttons
 document.getElementById('test1').addEventListener('click', test_1);
 document.getElementById('test2').addEventListener('click', test_2);
-document.getElementById('test3').addEventListener('click', test_3);
+document.getElementById('test3').addEventListener('click', swap_theme);
+
+// Function buttons
+theme_button.addEventListener('click', swap_theme);
 })();
 
 /******/ })()
